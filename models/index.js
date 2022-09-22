a3df67600ab5bb3716a5e19d7db6bc43273b00c8
colinmichael89 <colinmichael89@gmail.com>
2022-09-22 16:28:03 -0600

    fixed foreign key relationships in models/index

--- models/index.js
+++ models/index.js
@@ -17,10 +17,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  unique: false,
+  foreignKey: "product_id",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  unique: false,
+  foreignKey: "tag_id",
 });
 
 module.exports = {
