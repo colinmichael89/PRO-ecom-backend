fb5ec81192b4874301855c92c5f92b7ec4f3f2f6
colinmichael89 <colinmichael89@gmail.com>
2022-09-21 18:01:11 -0600

    category-routes still needs correct syntax to include associated products 'through:, as:

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,27 @@ const { Category, Product } = require("../../models");
 
 // Current location `/api/categories`
 
-router.get("/", (req, res) => {
-  // find all categories, include its associated Products
+// find all categories, include its associated Products
+router.get("/", async (req, res) => {
+  try {
+    const categoryData = await Category.findAll(req.body, {
+      include: [{ model: Product }], // through: ?? as: ??
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No categories found." });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// find one category by its `id` include its associated Products
 router.get("/:id", async (req, res) => {
-  // find one category by its `id` include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: "product_tags" }],
+      include: [{ model: Product, through: Category, as: "category_products" }],
     });
     if (!categoryData) {
       res.status(404).json({ message: "No category found with this id!" });
@@ -23,8 +35,8 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// create a new category
 router.post("/", async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -33,6 +45,7 @@ router.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
+
 // update a category by its `id` value
 router.put("/:id", async (req, res) => {
   try {
@@ -51,6 +64,7 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 // delete a category by its `id` value
 router.delete("/:id", async (req, res) => {
   try {
